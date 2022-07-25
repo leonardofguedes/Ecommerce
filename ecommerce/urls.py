60e63719a83cac5be3422692de75f4318286be22
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-25 11:50:02 -0300

    Adicionando urls de login e registro

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home_page, about, contact
+from .views import *
 
 
 urlpatterns = [
     path('', home_page),
     path('about/', about),
     path('contato/', contact),
+    path('login/', login_view),
+    path('register/', register_page)
 ]
