1505c7425e7a923b14869f8c48ed2c99ac736a93
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-25 14:31:34 -0300

    Adicionando apps aos settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Our Apps
+    'ecommerce',
+    'product',
 ]
 
 MIDDLEWARE = [
