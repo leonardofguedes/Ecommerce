bedaeec5aa881c5977c4978a6dec2c50aaa7bca1
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-25 15:31:15 -0300

    Criando o método view da lista de produtos adicionados

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+
+def product_list_view(request):
+    products = Product.objects.all()
+    context = {
+        'produtos': products
+    }
+    return render(request, "pages/products_list.html", context)
