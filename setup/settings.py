6bfc55670ca11b05ad26ad1835b9770eb5356839
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-25 17:06:59 -0300

    Refatorando settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -130,8 +130,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "global/static")
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
