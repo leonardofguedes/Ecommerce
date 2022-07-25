dfb0ff1affe2111c9e8448ecbb6a69b9557fae14
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-25 15:30:45 -0300

    Incluindo URL de product list na URL de setup

--- setup/urls.py
+++ setup/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('ecommerce.urls')),
+    path('produtos/', include('product.urls')),
     path('admin/', admin.site.urls),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
