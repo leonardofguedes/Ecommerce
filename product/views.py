23a4db1bb7330b36ea4e055815c8d968a7f35c2b
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-26 16:11:27 -0300

    Alterando o filtro do método em detail para que busque a exata slug

--- product/views.py
+++ product/views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.shortcuts import render
 from .models import Product
 
 
@@ -11,11 +10,11 @@ def product_list_view(request):
     }
     return render(request, "pages/products_list.html", context)
 
-def detail_view(request, id):
-    product = Product.objects.filter(id=id).first()
+def detail_view(request, slug):
+    product = Product.objects.filter(slug__iexact=slug).first()
     if not product:
         return render(request, "pages/detail.html", context={
-            'product': f'O produto com id {id} não existe'
+            'product': f'O produto não foi encontrado'
         })
     else:
         return render(request, "pages/detail.html", context={
