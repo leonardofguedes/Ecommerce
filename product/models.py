15ae0f85e557352c6135c66f2763403f7e8b3e2e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-26 15:07:55 -0300

    Adicionando método slugfy a class

--- product/models.py
+++ product/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Product(models.Model):
+    CATEGO = (
+        ('Vestuário', 'Vestuário'),
+        ('Calçados', 'Calçados'),
+        ('Acessórios', 'Acessórios'),
+        ('Eletrônicos', 'Eletrônicos'),
+        ('Livros', 'Livros'),
+        ('Usados', 'Usados'),
+        ('Desconhecido', 'Desconhecido')
+    )
     title = models.CharField(max_length=120)
+    category = models.CharField(choices=CATEGO, max_length=25, default='Desconhecido')
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to='products/%Y/%m/%d/', null=True, blank=True, default='')
@@ -12,3 +23,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'+f'{self.pk}'
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
+
+
+
+
