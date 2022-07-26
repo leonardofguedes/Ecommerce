73fd795a5f0107d22cf40977f7cf11c90cc1637a
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-26 10:05:15 -0300

    Adicionando published e on_top nos models

--- product/models.py
+++ product/models.py
@@ -6,6 +6,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to='products/%Y/%m/%d/', null=True, blank=True, default='')
+    published = models.BooleanField(default=False)
+    on_top = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
