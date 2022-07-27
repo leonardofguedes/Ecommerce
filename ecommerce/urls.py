f6f6d388afec1f7c2bc6788c43dd0f5561152cac
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-27 16:00:54 -0300

    Adicionando names para as urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('', home_page),
+    path('', home_page, name='home'),
     path('about/', about),
-    path('contato/', contact),
-    path('login/', login_view),
-    path('register/', register_page)
+    path('contato/', contact, name='contact'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_page, name='logout'),
+    path('register/', register_page, name='register'),
 ]
