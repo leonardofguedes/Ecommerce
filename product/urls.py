8b803a67481c3a754dbbbd664aed4b928c78047c
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-27 16:01:09 -0300

    Adicionando names para as urls

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', product_list_view),
-    path('<slug:slug>/', detail_view)
+    path('', product_list_view, name='products'),
+    path('<slug:slug>/', detail_view, name='detail')
 ]
