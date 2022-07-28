ed26a6877e76f5d15179c52a4f4dd5519f2fd625
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-28 18:12:19 -0300

    Criando o guest-register-view

--- ecommerce/views/login.py
+++ ecommerce/views/login.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.utils.http import url_has_allowed_host_and_scheme
 from ecommerce.forms.login_form import LoginForm
+from ecommerce.forms.account_form import GuestForm
+from ecommerce.models.account_models import GuestEmail
+
+
+def guest_register_view(request):
+    form = GuestForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
+    if form.is_valid():
+        email = form.cleaned_data.get("email")
+        new_guest_email = GuestEmail.objects.create(email=email)
+        request.session['guest_email_id'] = new_guest_email.id
+        if url_has_allowed_host_and_scheme(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+        else:
+            return redirect("/register/")
+    return redirect("/register/")
+
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -11,6 +34,10 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
             return redirect("/")
         else:
             print('Login Inválido')
