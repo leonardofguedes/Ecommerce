0ba2850a516185325236e5f76203a9846747e10a
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-28 10:56:36 -0300

    Refatorando as urls para que receba as views específicas

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import *
+from ecommerce.views.about import *
+from ecommerce.views.contact import *
+from ecommerce.views.home_page import *
+from ecommerce.views.login import *
+from ecommerce.views.logout import *
+from ecommerce.views.register import *
 
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('about/', about),
+    path('about/', about, name='about'),
     path('contato/', contact, name='contact'),
     path('login/', login_view, name='login'),
     path('logout/', logout_page, name='logout'),
