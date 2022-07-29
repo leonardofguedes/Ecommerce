1b657d271c276f295f0bca683d37de084e1e120b
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-29 18:05:36 -0300

    Registrando model Address

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -3,8 +3,11 @@ from .models.cart_models import Cart
 from .models.orders_models import Order
 from .models.billing_models import BillingProfile
 from .models.account_models import GuestEmail
+from .models.addresses_models import Address
+
 
 admin.site.register(Cart)
 admin.site.register(Order)
 admin.site.register(BillingProfile)
 admin.site.register(GuestEmail)
+admin.site.register(Address)
