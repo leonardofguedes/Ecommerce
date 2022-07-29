521820ba593e16bd410b5a715b01e04a9e2f0a90
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-29 18:14:01 -0300

    Adicionando paths

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,7 @@ from ecommerce.views.login import *
 from ecommerce.views.logout import *
 from ecommerce.views.register import *
 from ecommerce.views.cart import *
+from ecommerce.views.addresses import *
 
 
 urlpatterns = [
@@ -16,7 +17,11 @@ urlpatterns = [
     path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
     path('register/guest/', guest_register_view, name='guest_register'),
+    path('api/cart/', cart_detail_api_view, name='api-cart'),
     path('cart/', cart, name='cart'),
-    path('cart-update', cart_update, name='update'),
+    path('update/', cart_update, name='update'),
     path('checkout/', checkout_home, name='checkout'),
+    path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
+    path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
+    path('checkout/success/', checkout_done_view, name='success'),
 ]
