f5f7827eeba083762f4a4a9f3c367d3054120d3a
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-30 10:25:40 -0300

    Criando método que testa a url de detail page

--- tests/product/url/test_url.py
+++ tests/product/url/test_url.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class Urls(TestCase):
     def test_products_list_url(self):
         product_url = reverse('products')
@@ -10,7 +11,6 @@ class Urls(TestCase):
         search_url = reverse('search')
         self.assertEqual(search_url, '/produtos/search/')
 
-
-"""
-    path('<slug:slug>/', detail_view, name='detail'),
-"""
+    def test_detail_url(self):
+        detail_url = reverse('detail', kwargs={'slug': 'teste-slug'})
+        self.assertEqual(detail_url, '/produtos/teste-slug/')
