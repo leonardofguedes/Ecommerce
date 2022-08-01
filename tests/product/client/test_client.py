71f06c6fc35aa268392a3651dc08b0f09166ca82
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-01 16:04:52 -0300

    Importando o ProductBaseTest e testando o search com um produto criado

--- tests/product/client/test_client.py
+++ tests/product/client/test_client.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from product.urls import *
+from tests.product.base_test.test_base_product import *
 
 
-class ProductClientTest(TestCase):
+class ProductClientTest(ProductTestBase):
+    # status test
     def test_products_list_status(self):
         response = self.client.get(reverse('products'))
         self.assertEqual(response.status_code, 200)
@@ -15,7 +16,7 @@ class ProductClientTest(TestCase):
     def test_detail_status_with_no_product_registered(self):
         response = self.client.get(reverse('detail', kwargs={'slug': 'teste'}))
         self.assertEqual(response.status_code, 200)
-
+    # template test
     def test_products_list_template(self):
         response = self.client.get(reverse('products'))
         self.assertTemplateUsed(response, 'pages/products_list.html')
@@ -27,3 +28,10 @@ class ProductClientTest(TestCase):
     def test_search_template(self):
         response = self.client.get(reverse('search') + '?q=teste')
         self.assertTemplateUsed(response, 'pages/search.html')
+
+    # product made test
+    def test_search_with_product(self):
+        product = self.make_product()
+        search_url = reverse('search')
+        response1 = self.client.get(f'{search_url}?q=produto-teste')
+        self.assertIn(product, response1.context['products'])
