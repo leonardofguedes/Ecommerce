7d4b368be05c26f3687b05acd4e3add33846457d
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-02 17:14:16 -0300

    Refatorando para um teste parametrizado

---
+++ tests/product/models/test_models.py
@@ -0,0 +1,18 @@
+from tests.product.base_test.test_base_product import *
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+
+class ProductModelTeste(ProductTestBase):
+    def setUp(self) -> None:
+        self.product = self.make_product()
+        return super().setUp()
+
+    @parameterized.expand([
+        ('title', 120),
+        ('category', 25),
+    ])
+    def test_product_fields_max_lenght(self, field, max_lenght):
+        setattr(self.product, field, 'A' * (max_lenght + 1))
+        with self.assertRaises(ValidationError):
+            self.product.full_clean()
