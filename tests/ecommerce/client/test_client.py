c0b069e71d4d2e294d3b6113952bd170389db2c2
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-02 15:47:12 -0300

    Criando teste com client do template de cart e do checkout

--- tests/ecommerce/client/test_client.py
+++ tests/ecommerce/client/test_client.py
@@ -81,15 +81,11 @@ class EcommerceClientTest(TestCase):
         response = self.client.get(reverse('register'))
         self.assertTemplateUsed(response, 'pages/register.html')
 
+    def test_cart_template(self):
+        response = self.client.get(reverse('cart'))
+        self.assertTemplateUsed(response, 'pages/cart.html')
 
-        """
-        path('register/', register_page, name='register'),
-        path('register/guest/', guest_register_view, name='guest_register'),
-        path('api/cart/', cart_detail_api_view, name='api-cart'),
-        path('cart/', cart, name='cart'),
-        path('update/', cart_update, name='update'),
-        path('checkout/', checkout_home, name='checkout'),
-        path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
-        path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
-        path('checkout/success/', checkout_done_view, name='success'),
-        """
+    def test_checkout_template(self):
+        response = self.client.get(reverse('success'))
+        self.assertTemplateUsed(response, 'pages/checkout-done.html')
+
