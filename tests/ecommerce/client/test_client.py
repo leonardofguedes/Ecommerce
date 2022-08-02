2ebb6240176f41d8b075812c2c1060d261ae763d
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-02 15:17:22 -0300

    Criando teste com client do template home e about

--- tests/ecommerce/client/test_client.py
+++ tests/ecommerce/client/test_client.py
@@ -56,3 +56,26 @@ class EcommerceClientTest(TestCase):
         response = self.client.get(reverse('success'))
         self.assertEqual(response.status_code, 200)
 
+    #template test
+    def test_home_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'pages/home.html')
+
+    def test_about_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'pages/about.html')
+
+        """
+        path('contato/', contact, name='contact'),
+        path('login/', login_view, name='login'),
+        path('logout/', logout_page, name='logout'),
+        path('register/', register_page, name='register'),
+        path('register/guest/', guest_register_view, name='guest_register'),
+        path('api/cart/', cart_detail_api_view, name='api-cart'),
+        path('cart/', cart, name='cart'),
+        path('update/', cart_update, name='update'),
+        path('checkout/', checkout_home, name='checkout'),
+        path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
+        path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
+        path('checkout/success/', checkout_done_view, name='success'),
+        """
