6d0ee62eed5c256eda66194a52dacfa52b6fc34e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-03 10:50:34 -0300

    Criando o teste de error_messages do LoginForm

--- tests/ecommerce/forms/test_error_message.py
+++ tests/ecommerce/forms/test_error_message.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from ecommerce.forms.contact_form import ContactForm
 from ecommerce.forms.register_form import RegisterForm
+from ecommerce.forms.login_form import LoginForm
 from parameterized import parameterized
 
 
@@ -9,7 +10,7 @@ class ContactFormUnitTest(TestCase):
             ('nome', {'required': 'Obrigatório o preenchimento do nome'}),
             ('email', {'invalid': 'Digite um email válido!', 'required': 'This field is required.'}),
             ('mensagem', {'required': 'É obrigatório o preenchimento do campo mensagem!'}), ])
-    def test_fields_help_text(self, field, needed):
+    def test_fields_error_messages(self, field, needed):
         form = ContactForm()
         current = form[field].field.error_messages
         self.assertEqual(current, needed)
@@ -17,8 +18,21 @@ class ContactFormUnitTest(TestCase):
 
 class RegisterFormUniTest(TestCase):
     @parameterized.expand([
-        ('password', {'required': 'This field is required.'}), ])
-    def test_fields_help_text(self, field, needed):
+        ('password', {'required': 'This field is required.'}),
+        ('username', {'required': 'This field is required.'}),
+        ('password2', {'required': 'This field is required.'}),
+        ('email', {'required': 'This field is required.'}), ])
+    def test_fields_error_messages(self, field, needed):
         form = RegisterForm()
+        current = form[field].field.error_messages
+        self.assertEqual(current, needed)
+
+
+class LoginFormUniTest(TestCase):
+    @parameterized.expand([
+        ('password', {'required': 'This field is required.'}),
+        ('username', {'required': 'This field is required.'}),])
+    def test_fields_error_messages(self, field, needed):
+        form = LoginForm()
         current = form[field].field.error_messages
         self.assertEqual(current, needed)
