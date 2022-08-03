c5c25ee31640b3320ca0d2cad1e55095308b947a
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-03 10:39:29 -0300

    Criando o teste de error_messages do RegisterForm

--- tests/ecommerce/forms/test_error_message.py
+++ tests/ecommerce/forms/test_error_message.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from ecommerce.forms.contact_form import ContactForm
+from ecommerce.forms.register_form import RegisterForm
 from parameterized import parameterized
 
 
@@ -7,8 +8,17 @@ class ContactFormUnitTest(TestCase):
     @parameterized.expand([
             ('nome', {'required': 'Obrigatório o preenchimento do nome'}),
             ('email', {'invalid': 'Digite um email válido!', 'required': 'This field is required.'}),
-            ('mensagem', {'required':'É obrigatório o preenchimento do campo mensagem!'}),])
+            ('mensagem', {'required': 'É obrigatório o preenchimento do campo mensagem!'}), ])
     def test_fields_help_text(self, field, needed):
         form = ContactForm()
+        current = form[field].field.error_messages
+        self.assertEqual(current, needed)
+
+
+class RegisterFormUniTest(TestCase):
+    @parameterized.expand([
+        ('password', {'required': 'This field is required.'}), ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
         current = form[field].field.error_messages
         self.assertEqual(current, needed)
